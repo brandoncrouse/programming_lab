27e0456071d16d6764973c19bdc840deec4d579e
Brandon <brandon.crouse@ontariotechu.net>
2023-02-22 06:09:34 -0500

    dash polish

--- Programming_Lab_Game/Assets/Scripts/CharacterMovement.cs
+++ Programming_Lab_Game/Assets/Scripts/CharacterMovement.cs
@@ -8,34 +8,44 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private GameObject hairObject;
     public float yOffset, spriteRot;
     public float squashFactor, stretchFactor, ssLerpSpeed;
-    public GameObject sprite;
+    public GameObject sprite, dashGhostPrefab;
+    private GameObject dashGhost;
 
-    public float maxSpeed;
+    //movement
+    public float maxSpeed, coyoteTime;
     private float moveSpeed;
     private float horizontalMovement;
+    private bool canCoyote, coyoteJumping;
+
+    //components
     private Rigidbody2D rb;
     private SpriteRenderer srender;
 
+    //jumping and landing
     public float jumpForce, jumpDash;
     private bool justJumped = false, tryJump = false;
     private bool canLand = true;
     private bool willFlip = false;
 
+    //dash
     private bool canDash = true, isDashing;
     public float dashingPower = 24f, dashingTime = 0.2f, dashingCooldown = 1f;
     [SerializeField] private TrailRenderer tr;
 
+    //audio
     public AudioSource sfxSource, stepSource;
-    public AudioClip jumpSound, dashSound;
+    public AudioClip jumpSound, dashSound, zapSound;
 
+    //collisions
     public bool onGround = false;
     public Collider2D floorCollider;
     public ContactFilter2D floorFilter;
     private float dirFacing = 1f;
 
-    public ParticleSystem jumpDust, landDust, flipDust;
-    private TrailRenderer trail;
+    //particles
+    public ParticleSystem jumpDust, landDust, flipDust, dashDust;
 
+    //shaders for flash
     private Shader shaderGUItext, shaderSpritesDefault;
 
     private void Start()
@@ -46,13 +56,13 @@ public class CharacterMovement : MonoBehaviour
 
         //init components
         rb = GetComponent<Rigidbody2D>();
-        trail = GetComponent<TrailRenderer>();
         srender = sprite.GetComponent<SpriteRenderer>();
         moveSpeed = maxSpeed;
     }
 
     private void Update()
     {
+        hairSpriteOffsets();
 
         if (isDashing)
         {
@@ -87,9 +97,15 @@ public class CharacterMovement : MonoBehaviour
             {
                 stepSource.Stop();
             }
+            canCoyote = true;
         }
         else
         {
+            if (canCoyote)
+            {
+                canCoyote = false;
+                StartCoroutine(coyoteJump());
+            }
             stepSource.Stop();
             canLand = true;
         }
@@ -111,33 +127,59 @@ public class CharacterMovement : MonoBehaviour
                 }
             }
         }
+
+        //coyote jump
+        if (Input.GetKeyDown(KeyCode.Space) && coyoteJumping && rb.velocity.y < 0f)
+        {
+            justJumped = true;
+            sfxSource.PlayOneShot(jumpSound);
+        }
         
         //dash
-        if (canDash && Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        if (canDash)
         {
-            StartCoroutine(Dash());
-        }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                //dashGhost = (GameObject) Instantiate(dashGhostPrefab, transform.position, Quaternion.identity);
+            }
 
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Destroy(dashGhost);
+                StartCoroutine(Dash());
+            }
+        }
+        
+        //update dashghost position and sprite
+        if (dashGhost != null)
+        {
+            dashGhost.transform.position = new Vector3(transform.position.x + (1.5f * dirFacing), transform.position.y - 0.25f,1f);
+            dashGhost.transform.localScale = new Vector3(dirFacing, 1f, 1f);
+        }
         
     }
 
     private void FixedUpdate()
     {
+        //do none of this if dashing
         if (isDashing)
         {
             return;
         }
 
+        //set velocity based on movement code
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
 
         //lerp sprite squash n stretch
         sprite.transform.localScale = Vector3.Lerp(sprite.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * ssLerpSpeed);
 
+        //when jump
         if (justJumped)
         {
             sprite.transform.localScale = new Vector3(0.8f * stretchFactor,1.3f * stretchFactor,1f);
             moveSpeed = maxSpeed * jumpDash;
             jumpDust.Play();
+            landDust.Play();
             justJumped = false;
             rb.velocity = new Vector2(rb.velocity.x,jumpForce);
         }
@@ -146,6 +188,7 @@ public class CharacterMovement : MonoBehaviour
         
         willFlip = srender.flipX;
 
+        //update sprite and hair position when turning
         if (horizontalMovement > 0)
         {
             srender.flipX = false;
@@ -159,11 +202,60 @@ public class CharacterMovement : MonoBehaviour
             dirFacing = -1f;
         }
 
+        //create dust if player flipped
         if (srender.flipX != willFlip && onGround)
         {
             flipDust.Play();
         }
+        
+    }
 
+    private IEnumerator jumpTimer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        tryJump = false;
+    }
+
+    //thanks to Bendux on youtube for part of the dash function
+    private IEnumerator Dash()
+    {
+        sfxSource.PlayOneShot(dashSound);
+        sfxSource.PlayOneShot(zapSound);
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        //rb.gravityScale = 0;
+        rb.velocity = new Vector2(dirFacing * dashingPower, 0f);
+        dashDust.Play();
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+        StartCoroutine(blinkSprite());
+    }
+
+    private IEnumerator coyoteJump()
+    {
+        coyoteJumping = true;
+        yield return new WaitForSeconds(coyoteTime);
+        coyoteJumping = false;
+    }
+
+    private IEnumerator blinkSprite()
+    {
+        srender.material.shader = shaderGUItext;
+        srender.material.color = Color.white;
+        yield return new WaitForSeconds(0.07f);
+        srender.material.shader = shaderSpritesDefault;
+        srender.material.color = Color.white;
+    }
+
+    private void hairSpriteOffsets()
+    {
+        //set hair part offsets
         Vector2 currentOffset = Vector2.zero;
         currentOffset.x = -rb.velocity.x / 120;
         if (rb.velocity.y > 0)
@@ -177,39 +269,5 @@ public class CharacterMovement : MonoBehaviour
         currentOffset.y /= 1f - (-Mathf.Abs(rb.velocity.x) / 12);
 
         hairAnchor.partOffset = currentOffset;
-    }
-
-    private IEnumerator jumpTimer()
-    {
-        yield return new WaitForSeconds(0.1f);
-        tryJump = false;
-    }
-
-    //thanks to Bendux on youtube for the dash script
-    private IEnumerator Dash()
-    {
-        sfxSource.PlayOneShot(dashSound);
-        canDash = false;
-        isDashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0;
-        rb.velocity = new Vector2(dirFacing * dashingPower, 0f);
-        tr.emitting = true;
-        yield return new WaitForSeconds(dashingTime);
-        tr.emitting = false;
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
-        StartCoroutine(blinkSprite());
-    }
-
-    private IEnumerator blinkSprite()
-    {
-        srender.material.shader = shaderGUItext;
-        srender.material.color = Color.white;
-        yield return new WaitForSeconds(0.07f);
-        srender.material.shader = shaderSpritesDefault;
-        srender.material.color = Color.white;
     }
 }
